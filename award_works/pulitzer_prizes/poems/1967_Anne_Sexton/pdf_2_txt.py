439456702269d807eac0c5b095ee27602c1f4ed9
Seriainme <51992072+Seriainme@users.noreply.github.com>
2022-02-18 17:11:14 +0800

    Update pdf_2_txt.py

--- award_works/pulitzer_prizes/poems/1967_Anne_Sexton/pdf_2_txt.py
+++ award_works/pulitzer_prizes/poems/1967_Anne_Sexton/pdf_2_txt.py
@@ -4,4 +4,6 @@ doc = fitz.open('anne_sexton_poems.pdf')
 
 for page in doc:
     open('anne_sexton_poems.txt', 'a', encoding='utf-8').write(page.get_text() + '\n' + '*' * 50 + '\n')
-# 将pdf文件转为txt ，再去https://convertio.co/zh/txt-mobi/把txt转换成mobi格式
+#必须指定utf-8编码，不然会报错。
+    
+# 将pdf文件转为txt ，再去https://convertio.co/zh/txt-mobi/把txt转换成mobi格式。
